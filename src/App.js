a7bceb44aa65e9a88940f4d4bd86e0a13fa2cb35
Erdem Ceylan <erdemxceylan@gmail.com>
2022-08-09 00:20:40 +0300

    game over logic is completed

--- src/App.js
+++ src/App.js
@@ -1,67 +1,79 @@
 import React, { useState, useEffect } from 'react';
 import { header } from './assets/svgs';
-import './App.css';
 import { o } from './assets/svgs';
 import { x } from './assets/svgs';
+import './App.css';
 
 const X = 'X';
 const O = 'O';
-const boxStates = [null, X, O];
-const initialGameState = [];
+const initialBoxStates = [];
 for (let i = 0; i < 9; i++) {
-   initialGameState.push(boxStates[0]);
+   initialBoxStates.push(null);
 }
 
+const initialGameState = { isOver: false, boxIndexes: null, stateOfBoxes: null };
 const gameOverIndexes = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]];
+const gameOverStates = [O, X];
 
 const gameOver = array => {
-   const gameOverStates = [O, X];
+   let currentState = initialGameState;
 
-   gameOverIndexes.forEach(indexSet /* [0, 1, 2], ... */ => {
-      gameOverStates.forEach(state /* O, or X*/ => {
+   gameOverIndexes.forEach(indexSet => {
+      gameOverStates.forEach(state => {
          if (array[indexSet[0]] === state && array[indexSet[1]] === state && array[indexSet[2]] === state) {
-            console.log({ isGameOver: true, boxIndexes: indexSet, state: state });
-            return { isGameOver: true, boxIndexes: indexSet, box: state };
+            currentState = { isOver: true, boxIndexes: indexSet, stateOfBoxes: state };
          }
       });
    });
 
-   return { isGameOver: false, boxIndexes: null, box: null };
+   return currentState;
 };
 
 export default function App() {
+   const [boxStates, setBoxStates] = useState(initialBoxStates);
    const [gameState, setGameState] = useState(initialGameState);
 
    const switchState = (newBoxState, index) => {
-      setGameState(currentGameState => {
-         const newGameState = [...currentGameState];
-         newGameState[index] = newBoxState;
-         console.log(currentGameState, newGameState);
-         return newGameState;
+      setBoxStates(currentBoxStates => {
+         const newBoxStates = [...currentBoxStates];
+         newBoxStates[index] = newBoxState;
+         return newBoxStates;
       });
    };
 
    function clickHandler(boxState, index) {
-      switch (boxState) {
-         case null:
-            switchState(X, index);
-            break;
-         case X:
-            switchState(O, index);
-            break;
-         case O:
-            switchState(null, index);
-            break;
-         default:
-            alert('Something went wrong');
-            break;
+      if (gameState.isOver) {
+         setBoxStates(initialBoxStates);
+         setGameState(initialGameState);
+      } else {
+         switch (boxState) {
+            case null:
+               switchState(X, index);
+               break;
+            case X:
+               switchState(O, index);
+               break;
+            case O:
+               switchState(null, index);
+               break;
+            default:
+               alert('Something went wrong');
+               break;
+         }
       }
    }
 
-   const ticTacToe = gameState.map((state, index) => {
+   const ticTacToe = boxStates.map((state, index) => {
+      let className = state === X ? 'box box-x' : state === O ? 'box box-o' : 'box';
+
+      if (gameState.isOver && gameState.boxIndexes.includes(index)) {
+         const completionStyles = gameState.stateOfBoxes === X ? 'complete-x' : gameState.stateOfBoxes === O ? 'complete-o' : '';
+         className += ' ' + completionStyles;
+      }
+
       return (
          <div
-            className={state === X ? 'box box-x' : state === O ? 'box box-o' : 'box'}
+            className={className}
             onClick={() => clickHandler(state, index)}
          >
             {state === X ? x : state === O ? o : null}
@@ -70,15 +82,12 @@ export default function App() {
    });
 
    useEffect(() => {
-      const { isGameOver, boxIndexes, box } = gameOver(gameState);
+      const newGameState = gameOver(boxStates);
 
-      if (isGameOver) {
-         boxIndexes.forEach(index => {
-            console.log(ticTacToe[index]);
-         });
-         // setGameState(initialGameState);
+      if (newGameState.isOver) {
+         setGameState(newGameState);
       }
-   });
+   }, [boxStates]);
 
    return (
       <div className='container'>
